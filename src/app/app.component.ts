f28c89b60390f1f25b54af9c80351fd8ac74f417
RIshabh3192 <scorpiorishabh92@gmail.com>
2018-01-11 22:16:52 +0530

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from "@angular/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+   constructor() { }
+
+   ngOnInit(): void {}
+  title = 'Ernst and Young';
+  editingIndex: number = -1;
+  demo: Demo = new Demo();
+  targetPotentialArray: Demo[]= [{ status: "Research", companyInfo: "Banking", keyContacts: "Rishabh", financialPerformance: "A+" },
+  { status: "Approved", companyInfo: "IT", keyContacts: "Aggarwal", financialPerformance: "B+" },{ status: "Declined", companyInfo: "Networking", keyContacts: "Rishabh", financialPerformance: "C+" }]
+
+
+  editTargetPotential(index) {
+    this.editingIndex = index;
+    this.demo = this.targetPotentialArray[index]
+  }
+
+  deleteTargetPotential(index: number)
+  {
+     this.targetPotentialArray.splice(index, 1);
+     this.demo = new Demo()
+  }
+
+  postTargetPotential() {
+    if (this.editingIndex == -1) {
+      var demoTemp = new Demo();
+      demoTemp = this.demo;
+      this.targetPotentialArray.push(demoTemp);
+      this.demo = new Demo();
+    }
+    else {
+      var demoTemp = new Demo();
+      demoTemp =  this.demo;
+      this.targetPotentialArray.splice(this.editingIndex, 0, demoTemp);
+      this.targetPotentialArray.splice(this.editingIndex + 1, 1);
+      this.demo = new Demo()
+      this.editingIndex = -1;
+    }
+  }
+}
+
+
+export class Demo {
+  status: string;
+  companyInfo: string
+  keyContacts: string
+  financialPerformance: string
+
+  constructor() { }
 }
